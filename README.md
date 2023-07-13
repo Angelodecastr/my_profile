52803fd0b89b9a6ef56aae8f8ba1a0c29f84062d
Angelodecastr <angelodecastro206@gmail.com>
2023-07-14 02:21:55 +0800

    new1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Angelodecastr.github.io
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My profile</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    
+<header class="header">
+    <a href="#" class="logo">Angelo</a>
+
+    <nav class="navbar">  
+            <a href="#" class="active">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>      
+    </nav>
+</header>
+
+
+<section class="home">
+    <div class="home-content">
+        <h1>Hi Im Angelo DeCastro</h1>
+        <h2>A Font-end Developer</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, suscipit! Id provident quae maxime, fugit et consectetur cum asperiores tempore, quo praesentium assumenda exercitationem est adipisci maiores nostrum eum perspiciatis.</p>
+        <div class="button">
+            <a href="#" >Hire me</a>
+            <a href="#">Lets Talk</a>
+        </div>            
+    </div>
+
+
+    <div class= "icons">
+            <a href="#"><i class='bx bxl-facebook'></i></a>
+            <a href="#"><i class='bx bxl-twitter' ></i></a>
+            <a href="#"><i class='bx bxl-linkedin'></i></a>
+    </div>
+</section>
+
+
+
+
+
+    
+
+
+
+
+
+</body>
+</html>
